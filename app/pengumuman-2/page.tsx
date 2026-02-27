217c40ae02b44cc7c076c003a5ab0ab1970d074b
AlFakhir <smpialfakhir@gmail.com>
2026-02-27 17:17:30 +0700

    pengumunan 2

--- app/pengumuman-2/page.tsx
+++ app/pengumuman-2/page.tsx
@@ -12,7 +12,7 @@ const page = () => {
     <Layout>
       <iframe
         className="pdf_viewer"
-        src="https://drive.google.com/file/d/10s3J2sUbHPnVijA2_28khgBQ_fCin4XAgit/preview"
+        src="https://drive.google.com/file/d/10s3J2sUbHPnVijA2_28khgBQ_fCin4XA/preview"
         width="100%"
         height="1000"
         style={{ border: "none" }} // Tambahan agar tampilan lebih rapi
