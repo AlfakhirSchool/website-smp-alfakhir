94129acf9d06b565066c98876baa752464c04ec6
sdialfakhir <sdialfakhir@gmail.com>
2026-06-09 11:26:30 +0700

    fix: adjust google sheets append range to start at column B4

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -32,7 +32,7 @@ export async function POST(req: NextRequest) {
     const getRows = await sheets.spreadsheets.values.get({
       auth,
       spreadsheetId: process.env.GOOGLE_SHEET_ID,
-      range: "data-pendaftaran!A4:A1000", // Start from row 4 because of headers
+      range: "data-pendaftaran!B4:B1000", // Start from row 4 because of headers
     });
 
     const numRows = getRows.data.values ? getRows.data.values.length : 0;
@@ -40,7 +40,7 @@ export async function POST(req: NextRequest) {
 
     const response = await sheets.spreadsheets.values.append({
       spreadsheetId: process.env.GOOGLE_SHEET_ID,
-      range: "data-pendaftaran!A4:Z4",
+      range: "data-pendaftaran!B4:Y4",
       valueInputOption: "USER_ENTERED",
       requestBody: {
         values: [
